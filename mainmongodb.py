412411bdae166e4c599e9f5f5c83002954ac4733
Godfrey Purification <godfreypurification71@gmail.com>
2025-11-29 13:26:19 +0600

    Update mainmongodb.py

--- mainmongodb.py
+++ mainmongodb.py
@@ -2,8 +2,3 @@ from fastapi import FastAPI
 from routes.route import router
 app=FastAPI()
 app.include_router(router)
-
-
-
-
-# https://cloud.mongodb.com/v2/692a80fb888f5c48e4f33d83#/overview?automateSecurity=true&connectCluster=Cluster-fastapi
